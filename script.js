6cc49e540e31d38456e919a8dcc1cc02c0589f93
Dmitry Zhilin <unsait@inbox.ru>
2021-12-02 23:49:20 +0300

    fix: исправить ошибки по ревью

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-let popup_open = document.querySelector('.profile__edit-button');
-let popup_active = document.querySelector('.popup');
-let popup_close = document.querySelector('.popup-container__exite');
-let name_profile = document.querySelector('.profile__name');
-let status_profile = document.querySelector('.profile__status');
-let input_name = document.querySelector('.popup-container__name');
-let input_status = document.querySelector('.popup-container__status');
-let save = document.querySelector('.popup-container__save');
+let popupOpenButton = document.querySelector('.profile__edit-button');
+let popupCloseButton = document.querySelector('.popup__exite');
+let popupActive = document.querySelector('.popup');
+let nameProfile = document.querySelector('.profile__name');
+let statusProfile = document.querySelector('.profile__status');
+let inputName = document.querySelector('.popup__input_type_name');
+let inputStatus = document.querySelector('.popup__input_type_stasus');
+let sumbitBottom = document.querySelector('.popup__sumbit');
+let formProfile = document.querySelector('.popup__form');
 
-popup_open.addEventListener('click', function () {
-  popup_active.classList.toggle('popup__click_active');
-});
+function popupOpen() {
+  popupActive.classList.toggle('popup_click_active');
+};
 
-popup_close.addEventListener('click', function () {
-  popup_active.classList.toggle('popup__click_active');
-});
+function popupClose() {
+  popupActive.classList.toggle('popup_click_active');
+};
 
-save.addEventListener('click', function () {
-  name_profile.textContent = `${input_name.value}`;
-  status_profile.textContent = `${input_status.value}`;
-  popup_active.classList.toggle('popup__click_active');
-});
+function profileSumbitHandler(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = `${inputName.value}`;
+  statusProfile.textContent = `${inputStatus.value}`;
+}
+
+popupOpenButton.addEventListener('click', popupOpen);
+popupCloseButton.addEventListener('click', popupClose);
+sumbitBottom.addEventListener('click', popupClose);
+formProfile.addEventListener('submit', profileSumbitHandler);
